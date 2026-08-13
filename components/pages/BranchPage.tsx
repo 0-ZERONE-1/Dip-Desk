1b6ca550b84b12140486fe6c027523c2e8c58e7c
ZERONE <nikhil.hub.github@gmail.com>
2026-08-13 12:12:47 +0530

    gug fix

--- components/pages/BranchPage.tsx
+++ components/pages/BranchPage.tsx
@@ -96,7 +96,7 @@ export default function BranchPage({ branchSlug }: Props) {
       </div>
 
       {/* Semester Grid */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6 gap-4 sm:gap-5">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-5">
         {[1, 2, 3, 4, 5, 6].map((sem, i) => (
           <motion.div
             key={sem}
